e95fb187e2bef3ffa283f705808088637d0d1c6a
Akarsh <akarshmishra050@gmail.com>
2026-04-12 03:22:50 +0530

    auth_feat login setup

--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.js";
+import { registerUser , loginUser } from "../controllers/auth.js";
 import uploadFile from "../middleware/multer.js";
 
 // import {
@@ -13,7 +13,7 @@ import uploadFile from "../middleware/multer.js";
 const router = express.Router();
 
 router.post("/register", uploadFile, registerUser);
-
+router.post("/login", loginUser);
 export default router;
 
 
