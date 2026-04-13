6f398624a05e53e4e757f6bb78434796b6f9d31b
Akarsh <akarshmishra050@gmail.com>
2026-04-14 01:39:08 +0530

    user profile update setup

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,25 +1,26 @@
 import express from "express";
-// import { isAuth } from "../middlewares/auth.js";
-// import {
+import { isAuth } from "../middlewares/auth.js";
+
+ import {
 //   addSkillToUser,
 //   applyForJob,
 //   deleteSkillFromUser,
 //   getAllaplications,
-//   getUserProfile,
-//   myProfile,
-//   updateProfilePic,
-//   updateResume,
-//   updateUserProfile,
-// } from "../controllers/user.js";
-// import uploadFile from "../middlewares/multer.js";
+   getUserProfile,
+   myProfile,
+   updateProfilePic,
+   updateResume,
+   updateUserProfile,
+ } from "../controllers/user.js";
+import uploadFile from "../middlewares/multer.js";
 
 const router = express.Router();
 
-// router.get("/me", isAuth, myProfile);
-// router.get("/:userId", isAuth, getUserProfile);
-// router.put("/update/profile", isAuth, updateUserProfile);
-// router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
-// router.put("/update/resume", isAuth, uploadFile, updateResume);
+router.get("/me", isAuth, myProfile);
+router.get("/:userId", isAuth, getUserProfile);
+router.put("/update/profile", isAuth, updateUserProfile);
+router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
+router.put("/update/resume", isAuth, uploadFile, updateResume);
 // router.post("/skill/add", isAuth, addSkillToUser);
 // router.put("/skill/delete", isAuth, deleteSkillFromUser);
 // router.post("/apply/job", isAuth, applyForJob);
