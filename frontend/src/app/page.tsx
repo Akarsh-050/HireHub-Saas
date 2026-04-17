de5ec753fd638126eda885502046f15e4d54aea1
Akarsh <akarshmishra050@gmail.com>
2026-04-18 00:36:41 +0530

    frontend setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button  } from '@/components/ui/button'
+
+const Home = () => {
+  return (
+    <div>
+
+      <Button variant={"destructive"} > hli </Button> 
+    </div>
+  )
+}
+
+export default Home
